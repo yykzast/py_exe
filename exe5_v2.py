6ebd84a5f7ec017e5e4d135d26a6bc509d6c3101
Yerzhan <yykzast@yandex.ru>
2020-03-22 08:53:20 +0600

    two player plays

--- exe5_v2.py
+++ exe5_v2.py
@@ -1,68 +1,58 @@
 
-# find XO in the field and returns their index
-def find_XO(f,xo):
+# this function make a move in the field according to [X] or [O]. returns new field and result of WIN, DRAW
+def MOVE(p,f):
+    move = None
     f_index = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
-    p = []
-    k = 0
-    for a in f:
-        i = 0
-        for b in a:
-            if b == xo:
-                p.append(f_index[k][i])
-            i = i + 1
-        k = k + 1
-    return p
+    win = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+    xy = {0: (0, 0), 1: (0, 1), 2: (0, 2), 3: (1, 0), 4: (1, 1), 5: (1, 2), 6: (2, 0), 7: (2, 1), 8: (2, 2)}
+    if_win = False
+    if_draw = False
 
-# decide whether draw or not
-def DRAW(f):
-    i = 0
-    for a in f:
-        for b in a:
-            if b in ['[X]','[O]']:
-                i = i + 1
-    if i == 9: return True
-    else: return False
+    while move not in range(9) or f[xy[move][0]][xy[move][1]] in ['[X]','[O]']:
+        try: move = int(input(p + "вставьте: "))
+        except: print("Введите числовое значение!")
+    f[xy[move][0]][xy[move][1]] = p
+    for i in f: print(''.join(i))
+
+    place = []
+    i1 = 0
+    for a1 in f:
+        i2 = 0
+        for b1 in a1:
+            if b1 == p:
+                place.append(f_index[i1][i2])
+            i2 = i2 + 1
+        i1 = i1 + 1
+
+    for item in win:
+        if all(e in place for e in item):
+            if_win = True
+            print(p + " выиграл!")
+            break
+    k = 0
+    for a2 in f:
+        for b2 in a2:
+            if b2 in ['[X]','[O]']:
+                k = k + 1
+    if k == 9:
+        if_draw = True
+        print("Ничья!")
+
+    return f, if_win, if_draw
 
 x = '[X]'
 o = '[O]'
-WIN = [[0, 1, 2],[3, 4, 5],[6, 7, 8],[0, 3, 6],[1, 4, 7],[2, 5, 8],[0, 4, 8],[2, 4, 6]]
-MOVE = {0:(0, 0), 1:(0, 1), 2:(0, 2), 3:(1, 0), 4:(1, 1), 5:(1, 2), 6:(2, 0), 7:(2, 1), 8:(2, 2)}
 field_numbers = [['[0]','[1]','[2]'],['[3]','[4]','[5]'],['[6]','[7]','[8]']]
 for i in field_numbers: print(''.join(i))
 
 field = [['[ ]','[ ]','[ ]'],['[ ]','[ ]','[ ]'],['[ ]','[ ]','[ ]']]
-IF_WIN = False
 
 while True:
 
-    playerX = None
-    while playerX not in range(9) or field[MOVE[playerX][0]][MOVE[playerX][1]] in [x, o]:
-        try: playerX = int(input("(X): "))
-        except: print("Введите числовое значение!")
-    field[MOVE[playerX][0]][MOVE[playerX][1]] = x
-    for i in field: print(''.join(i))
-    for item in WIN:
-        if all(e in find_XO(field,x) for e in item):
-            IF_WIN = True
-            print("(X) выиграл!")
-            break
-    if IF_WIN == True: break
-    elif DRAW(field):
-        print("Ничья!")
+    field = MOVE(x,field)[0]
+    if MOVE(x,field)[1] or MOVE(x,field)[2]:
         break
 
-    playerO = None
-    while playerO not in range(9) or field[MOVE[playerO][0]][MOVE[playerO][1]] in [x, o]:
-        try: playerO = int(input("(O): "))
-        except: print("Введите числовое значение!")
-    field[MOVE[playerO][0]][MOVE[playerO][1]] = o
-    for i in field: print(''.join(i))
-    for item in WIN:
-        if all(e in find_XO(field,o) for e in item):
-            IF_WIN = True
-            print("(O) выиграл!")
-            break
-    if IF_WIN == True: break
-    elif DRAW(field):
-        print("Ничья!")
+    field = MOVE(o,field)[0]
+    if MOVE(o,field)[1] or MOVE(o,field)[2]:
         break
